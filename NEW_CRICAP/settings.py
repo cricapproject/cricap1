0ee6f440cdfb0f38f42c4c0002f3b71c3b98bf0f
cricapproject <cricapproject@gmail.com>
2025-10-14 15:49:18 +0700

    settings.py

--- NEW_CRICAP/settings.py
+++ NEW_CRICAP/settings.py
@@ -120,12 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / 'mystaticfiles'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
